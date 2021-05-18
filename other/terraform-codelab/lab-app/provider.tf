81ff524bf36687d0553a24915f9a86a2e3a929cf
dylanmpeck <dylanmpeck@google.com>
2021-05-18 22:11:40 +0000

    terraform fmt source

--- other/terraform-codelab/lab-app/provider.tf
+++ other/terraform-codelab/lab-app/provider.tf
@@ -15,7 +15,7 @@
  */
 
 provider "google" {
-  version = "~> 2.5"
-  project = "${var.project_id}"
-  credentials = "${file("credentials.json")}"
+  version     = "~> 2.5"
+  project     = var.project_id
+  credentials = file("credentials.json")
 }
--- other/terraform-codelab/lab-app/vm.tf
+++ other/terraform-codelab/lab-app/vm.tf
@@ -16,24 +16,24 @@
 
 data "google_compute_image" "apache" {
   family  = "my-apache-webserver"
-  project = "${var.project_id}"
+  project = var.project_id
 }
 
 resource "google_compute_instance" "app" {
   name         = "my-app-instance"
-  project      = "${var.project_id}"
+  project      = var.project_id
   machine_type = "n1-standard-2"
   zone         = "us-west1-a"
 
   boot_disk {
     initialize_params {
-      image = "${data.google_compute_image.apache.self_link}"
+      image = data.google_compute_image.apache.self_link
     }
   }
 
   network_interface {
     subnetwork         = data.terraform_remote_state.network.outputs.my_subnet_name
-    subnetwork_project = "${var.project_id}"
+    subnetwork_project = var.project_id
 
     access_config {
       # Include this section to give the VM an external ip address
